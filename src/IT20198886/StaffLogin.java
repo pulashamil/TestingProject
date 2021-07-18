d1ad93e90fffe74223cc757f3c2ce45f7b705c0a
Pula Shamil <pulasthishamil@gmail.com>
2021-07-18 13:35:23 +0530

    First Commit

---
+++ src/IT20198886/StaffLogin.java
@@ -0,0 +1,43 @@
+package IT20198886;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class StaffLogin {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StaffLogin window = new StaffLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StaffLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 580, 437);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
